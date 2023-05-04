18f24ab33469a89d64e393f387fa6c494545c9ea
monstergog <amn002@ucsd.edu>
2023-05-04 03:54:05 -0700

    added current date to screen + event listener for saving schedule + partial local storage functionality

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,12 +2,41 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  $('#currentDay').text(dayjs().format('[Today is: ]dddd, MMMM D[th]'));
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
+  var schedule = [];
+  
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  $('button').click(function() {
+    var task = {
+      time: $(this).parent().attr('id').slice(5),
+      note: $(this).siblings('textarea').val()
+    }
+    if (schedule == ''  ) {
+      schedule.push(task);
+      console.log(schedule);
+      console.log('Middle: ' + Math.floor(schedule.length / 2));
+    } else if (schedule[Math.floor(schedule.length / 2)].time === task.time) {
+      console.log('Middle: ' + Math.floor(schedule.length / 2));
+      schedule[Math.floor(schedule.length / 2)] = task;
+      console.log('replaced', schedule);
+    } else if (schedule[Math.floor(schedule.length / 2)].time < task.time) {
+      console.log('Middle: ' + Math.floor(schedule.length / 2));
+      schedule.splice(schedule[Math.floor(schedule.length / 2)], 0, task);
+      console.log('added before', schedule);
+    } else {
+      console.log('Middle: ' + Math.floor(schedule.length / 2));
+      schedule.splice(schedule[Math.floor(schedule.length / 2 + 1)], 0, task);
+      console.log('added after', schedule);
+    }
+    
+    // localStorage.setItem('schedule', schedule);
+  })
   
 
   // TODO: Add code to apply the past, present, or future class to each time
